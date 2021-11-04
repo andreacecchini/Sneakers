01f0afbf45f8e0ba3db05a1aad6e4ac374540d64
Andrea Cecchini <63281186+andreacecchini@users.noreply.github.com>
2021-11-04 14:35:55 +0100

    Version 1.0
    Abbiamo da fixare il problema nel quale se vengono generati caratteri speciali nella stringa cryptata il programma non riesce a sostituirli.

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 #include <stdlib.h>
 #define LENGHT 100
 
+
+//DICHIARAZIONE DELLE FUNZIONI
+
 //Questa funzione permette di ottenere una stringa pseudo-casuale
 void randomString(char *, size_t);
 /*
@@ -20,11 +23,19 @@ Questa funzione permette di simulare la 'decryptazione' della stringa casuale
 in quella inserita dall'utente.
 */
 void decrypted(char *, char *);
-
+/*
+Questa funzione permette di controllare se la stringa e` stata "decryptata" completamente nell'originale.
+Richiede come parametri due puntatori a carattere, in questo ordine
+- char * : puntatore a carattere della stringa da "decryptare"
+- char * : puntatore a carattere della stringa originale
+*/
+bool isDecrypted(char *, char *);
+//Questa funzione permette di effettuare un delay in secondi esplicitati nell'argomento
 void waiting(float seconds);
 
 
 int main(){
+    //LENGHT + \0
     char initString[LENGHT+1];
     char cryptedString[LENGHT+1];
     //Inserimento della stringa di input da parte dell'utente
@@ -33,9 +44,11 @@ int main(){
     randomString(cryptedString, strlen(initString));
     initString [strcspn(initString, "\n\r")] = 0;
     cryptedString [strcspn(cryptedString, "\n\r")] = 0;
+    //Chiamata alla funzione di "decryptazione"
     decrypted(cryptedString, initString);
     return 0;
 }
+
 
 
 void randomString(char *cryptedString, size_t lenght) {
@@ -58,9 +71,8 @@ bool isDecrypted(char *cryptedString, char *original){
     return true;
 }
 
-
-//Questa funzione permette di simulare la decriptazione del messaggio nell'input iniziale dell'utente
 void decrypted(char *cryptedString, char *result){
+    srand(time(NULL));
     int index[strlen(cryptedString)];
     int i;
     int count= 0;
@@ -69,36 +81,33 @@ void decrypted(char *cryptedString, char *result){
     //Ritorno alla parte inziale della stringa
     do{
         printf("\r");
+        /*
+        Generazione casuale dell'indice della stringa che verra decriptata
+        Questo accesso alla stringa permette di dare l'effetto ottico di decryptazione dei caratteri in modo non sequeziale.
+        */
         i = rand()%(strlen(result));
+        //Se i caratteri sono uguali non si ha la necessita` di riscreve la stringa, e per non sprecare istruzioni attraverso continue si esegue un salto alla fine del for.
+        if(cryptedString[i]==result[i])
+            continue;
+        //Assegnazione del carattere "decryptato" alla stringa stampata
         cryptedString[i] = result[i];
         for(int j = 0; j < strlen(result); j++){
+            /*
+             Se la stringa non e` stata "decryptata" e i valori sono diversi allora viene generato un carattere casuale da mettere nella cella j.
+             Queste istruzioni mi permettono di dare l'effetto ottico desiderato, come da clip.
+            */
+            if(cryptedString[j]!= result[j])
+                cryptedString[j] = 'A' + (rand() % 26);
+            //Stampa carattere per carattere la stringa con un carattere "decryptato" in piu`
             printf("%c", cryptedString[j]);
         }
-        waiting(0.1);
+        //0.05s delay per generare l'effetto ottico e non avere il risultato immediatamente
+        waiting(0.05);
     }while(!isDecrypted(cryptedString, result));
 }
-
-
-
-
-    /*
-    do{
-        printf("\r");
-        for (int k = 0; k < strlen(cryptedString); k++) {
-            printf("%c", cryptedString[k]);
-        }
-        i = rand()%(strlen(result));
-        if(cryptedString[i] != result[i])
-            count++;
-        cryptedString[i] = result[i];
-        waiting(0.1);
-    }while(count < strlen(cryptedString));
-    */
-
-
-
 
 void waiting(float seconds){
     clock_t now = clock();
     for( ; clock() - now  < seconds * CLOCKS_PER_SEC; );  // Wait seconds
 }
+
